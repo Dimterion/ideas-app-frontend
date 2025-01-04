c984c61cbbcffce56ef56e60bdc15b71463a98e8
Dmitrii <dimterion59@gmail.com>
2025-01-04 10:01:13 +0100

    add additional font

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import { lusitana } from "./ui/fonts";
 import styles from "@/app/ui/home.module.css";
 
 export default function Page() {
@@ -8,7 +9,9 @@ export default function Page() {
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52"></div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <p
+            className={`${lusitana.className} text-xl text-gray-800 antialiased md:text-3xl md:leading-normal`}
+          >
             Welcome.
           </p>
           {/* CSS Module styles */}
