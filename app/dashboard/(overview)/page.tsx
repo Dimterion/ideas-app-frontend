63b9e2e52fa2afdd6cdd5d81c83d61541a870cfb
Dmitrii <dimterion59@gmail.com>
2025-01-11 10:46:41 +0100

    add suspense to LatestInvoices

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,12 +2,14 @@ import { Suspense } from "react";
 import { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
-import { RevenueChartSkeleton } from "@/app/ui/skeletons";
-import { fetchCardData, fetchLatestInvoices } from "@/app/lib/data";
+import {
+  LatestInvoicesSkeleton,
+  RevenueChartSkeleton,
+} from "@/app/ui/skeletons";
+import { fetchCardData } from "@/app/lib/data";
 import { lusitana } from "@/app/ui/fonts";
 
 export default async function Page() {
-  const latestInvoices = await fetchLatestInvoices();
   const {
     totalPaidInvoices,
     totalPendingInvoices,
@@ -34,7 +36,9 @@ export default async function Page() {
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
