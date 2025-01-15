0a164103b702f747f5a02032d046c8a96696a385
Dmitrii <dimterion59@gmail.com>
2025-01-15 11:44:26 +0100

    add try-catch statements to server actions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -25,10 +25,16 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
 
-  await sql`
+  try {
+    await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+  } catch (error) {
+    return {
+      message: "Database error: could not create invoice.",
+    };
+  }
 
   revalidatePath("/dashboard/invoices");
 
@@ -44,11 +50,17 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   const amountInCents = amount * 100;
 
-  await sql`
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
+  } catch (error) {
+    return {
+      message: "Database error: could not update invoice.",
+    };
+  }
 
   revalidatePath("/dashboard/invoices");
 
@@ -56,7 +68,15 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
 
-  revalidatePath("/dashboard/invoices");
+    revalidatePath("/dashboard/invoices");
+
+    return { message: "Invoice deleted." };
+  } catch (error) {
+    return {
+      message: "Database error: could not delete invoice.",
+    };
+  }
 }
