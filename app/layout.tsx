7f33a5d7ee38240f55ecf60eeaed1e752d20b1f6
Dmitrii <dimterion59@gmail.com>
2025-01-20 10:44:23 +0100

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
+import { Metadata } from "next";
 import { inter } from "@/app/ui/fonts";
 import "@/app/ui/global.css";
+
+export const metadata: Metadata = {
+  title: "Finboard",
+  description: "Finboard project.",
+  metadataBase: new URL("https://finboard-rho.vercel.app/"),
+};
 
 export default function RootLayout({
   children,
